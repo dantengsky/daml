875f6fba31446516d1afc8b102424571f881e358
Nick Chapman <nick.chapman@digitalasset.com>
2020-06-12 10:26:56 +0100

    rework the ANF compiler pass to be semantically correct; add trampolines to avoid stack overflow

--- daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Anf.scala
+++ daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/Anf.scala
@@ -4,124 +4,135 @@
 package com.daml.lf.speedy
 
 /**
-  * ANF based AST for the speedy interpreter.
+  * Transformation to ANF based AST for the speedy interpreter.
   */
 import com.daml.lf.speedy.SExpr._
+
+import scala.annotation.tailrec
 
 object Anf {
 
   case class CompilationError(error: String) extends RuntimeException(error, null, true, false)
 
-  case class Env(absMap: Map[Int, Int], newDepth: Int)
+  case class DepthE(n: Int)
+  case class DepthA(n: Int)
 
-  val initEnv = Env(absMap = Map.empty, newDepth = 0)
+  case class Env(absMap: Map[DepthE, DepthA], oldDepth: DepthE)
 
-  def remapRelStackOffset(oldDepth: Int, env: Env, oldRel: Int): Int = {
-    val oldAbs = oldDepth - oldRel
-    env match {
-      case Env(absMap,newDepth) =>
-        absMap.get(oldAbs) match {
-          case None =>
-            throw CompilationError(s"remapStackLocation(oldDepth=$oldDepth,env=$env,oldRel=$oldRel,oldAbs=$oldAbs)")
-          case Some(newAbs) =>
-            val newRel = newDepth - newAbs
-            //println(s"remapStackLocation(oldDepth=$oldDepth,env=$env,oldRel=$oldRel -> oldAbs=$oldAbs -> newAbs=$newAbs -> newRel=$newRel")
-            newRel
-        }
-    }
-  }
+  val initEnv = Env(absMap = Map.empty, oldDepth = DepthE(0))
 
-  def trackBindings(oldDepth: Int, env: Env, n: Int) : Env = {
+  def trackBindings(depth: DepthA, env: Env, n: Int) : Env = {
     if (n == 0) {
       env
     } else {
-      env match {
-        case Env(absMap,newDepth) =>
-          val extra = (0 to n-1).map(i => (oldDepth + i, newDepth + i))
-          val newEnv = Env(absMap ++ extra,newDepth + n)
-          //println(s"trackBindings(oldDepth=$oldDepth,env=$env,n=$n) -> newEnv=$newEnv")
-          newEnv
-      }
+      val extra = (0 to n-1).map(i => (DepthE(env.oldDepth.n + i), DepthA(depth.n + i)))
+      Env(absMap = env.absMap ++ extra, oldDepth = DepthE(env.oldDepth.n + n))
     }
   }
 
-  case class NewBinding(madeAtDepth: Int)
+  type Anf = SExpr
 
-  def makeNewBinding(env: Env) : (Env,NewBinding) = {
-    env match {
-      case Env(absMap,newDepth) =>
-        val res = (Env(absMap,newDepth + 1), NewBinding(madeAtDepth = newDepth))
-        res
+  sealed trait Trampoline[T] {
+    @tailrec
+    def bounce : T = this match {
+      case Land(x) => x
+      case Bounce(continue) => continue().bounce
     }
   }
 
-  type Atom = Either[SExprAtomic,NewBinding]
+  final case class Land[T](x: T) extends Trampoline[T]
+  final case class Bounce[T](continue: () => Trampoline[T]) extends Trampoline[T]
 
+  type Res = Trampoline[Anf]
+  type K[T] = ((DepthA, T) => Res)
 
-  type Res = SExpr
-  type K[T] = ((Env, T) => Res)
+  case class AbsBinding(abs: DepthA)
 
-
-  // TODO: inline?
-  def relocateExpAtomic(depth: Int, env: Env, exp: SExprAtomic): SExprAtomic = exp match {
-    case SELocS(n) => SELocS (remapRelStackOffset(depth, env, n))
-    case atomic => atomic
+  def makeAbsoluteB(env: Env, rel: Int) : AbsBinding = {
+    val oldAbs = DepthE(env.oldDepth.n - rel)
+    env.absMap.get(oldAbs) match {
+      case None => throw CompilationError(s"makeAbsoluteB(env=$env,rel=$rel)")
+      case Some(abs) => AbsBinding(abs)
+    }
   }
 
-  def relocateAtom(depth: Int, env: Env)(atom: Atom) : SExprAtomic = {
-    val exp =
-      atom match {
-        case Left(exp) => relocateExpAtomic(depth,env,exp)
-        case Right(NewBinding(madeAtDepth)) =>
-          val rel: Int = env.newDepth - madeAtDepth
-        SELocS(rel)
-      }
-    //println(s"expandAtom(madeAtDepth=$madeAtDepth), env.newDepth=${env.newDepth} -> rel=$rel")
-    //println(s"relocateAtom: $atom -> $exp")
-    exp
+  def makeRelativeB(depth: DepthA, binding: AbsBinding) : Int = {
+    (depth.n - binding.abs.n)
+  }
+
+  type AbsAtom = Either[SExprAtomic,AbsBinding]
+
+  def makeAbsoluteA(env: Env, atom: SExprAtomic) : AbsAtom = atom match {
+    case SELocS(rel) => Right (makeAbsoluteB(env,rel))
+    case x: SELocA => Left(x)
+    case x: SELocF => Left(x)
+    case x: SEBuiltin => Left(x)
+    case x: SEValue => Left(x)
+  }
+
+  def makeRelativeA(depth: DepthA)(atom: AbsAtom) : SExprAtomic = atom match {
+    case Left(x:SELocS) => throw CompilationError(s"makeRelativeA: unexpected: $x")
+    case Left(atom) => atom
+    case Right(binding) => SELocS(makeRelativeB(depth,binding))
+  }
+
+  def relocateA(depth: DepthA, env: Env)(atom: SExprAtomic): SExprAtomic = {
+    makeRelativeA(depth)(makeAbsoluteA(env, atom))
+  }
+
+  // TODO: ? inline AbsLoc, makeAbsolute/Relative(L) -- all we need in relocateL
+  type AbsLoc = Either[SELoc,AbsBinding]
+
+  def makeAbsoluteL(env: Env, loc: SELoc) : AbsLoc = loc match {
+    case SELocS(rel) => Right (makeAbsoluteB(env,rel))
+    case x: SELocA => Left(x)
+    case x: SELocF => Left(x)
+  }
+
+  def makeRelativeL(depth: DepthA)(loc: AbsLoc) : SELoc = loc match {
+    case Left(x:SELocS) => throw CompilationError(s"makeRelativeL: unexpected: $x")
+    case Left(loc) => loc
+    case Right(binding) => SELocS(makeRelativeB(depth,binding))
+  }
+
+  def relocateL(depth: DepthA, env: Env)(loc: SELoc): SELoc = {
+    makeRelativeL(depth)(makeAbsoluteL(env, loc))
   }
 
   // flatten* -- non-continuation entry points
 
-  def flattenEntry(exp: SExpr): SExpr = {
-    val depth = 0
+  def flattenEntry(exp: SExpr): Anf = {
+    val depth = DepthA(0)
     val env = initEnv
     flattenExp(depth,env,exp)
   }
 
-  def flattenExp(depth: Int, env: Env, exp: SExpr): SExpr = {
-    atomizeExp(depth, env, exp, { case (env,atom) =>
-      relocateAtom(depth,env)(atom)
-    })
+  def flattenExp(depth: DepthA, env: Env, exp: SExpr): Anf = {
+    val k0 : K[Anf] = {
+      case (depth@_,anf) => Land(anf)
+    }
+    transformExp(depth, env, exp, k0).bounce
   }
 
   // transform* -- continuation entry points
 
   //TODO: inline?
-  def transformLets(depth: Int, env: Env, rhss: List[SExpr], body: SExpr, k: K[SExpr]): Res = rhss match {
+  def transformLets(depth: DepthA, env: Env, rhss: List[SExpr], body: SExpr, k0: K[SExpr]): Res = rhss match {
     case rhs::rhss =>
-      atomizeExp(depth, env, rhs, { case (env,rhs) =>
-        val rhs1 = relocateAtom(depth,env)(rhs)
-        SELet(Array(rhs1),
-          transformLets(depth + 1, trackBindings(depth,env,1), rhss, body, k))
+      transformExp(depth, env, rhs, { case (depth,rhs) =>
+        Land(SELet(Array(rhs),
+          transformLets(DepthA(depth.n + 1), trackBindings(depth,env,1), rhss, body, k0).bounce
+        ))
       })
     case Nil =>
-      transformExp(depth,env,body, { case (env,exp) =>
-        k(env,exp)
-      })
+      transformExp(depth,env,body,k0)
   }
 
   //TODO: inline
-  def remapLocation(depth: Int, env: Env)(loc: SELoc): SELoc = loc match {
-    case SELocS(n) => SELocS(remapRelStackOffset(depth, env, n))
-    case SELocA(_) | SELocF(_) => loc
-  }
-
-  //TODO: inline
-  def flattenAlts(depth: Int, env: Env, alts: Array[SCaseAlt]): Array[SCaseAlt] = {
+  def flattenAlts(depth: DepthA, env: Env, alts: Array[SCaseAlt]): Array[SCaseAlt] = {
     alts.map{ case SCaseAlt(pat,body0) =>
       val n = patternNArgs(pat)
-      val body = flattenExp(depth + n, trackBindings(depth, env, n), body0)
+      val body = flattenExp(DepthA(depth.n + n), trackBindings(depth, env, n), body0)
       SCaseAlt(pat,body)
     }
   }
@@ -132,19 +143,19 @@ object Anf {
     case SCPCons => 2
   }
 
+  def transformExp(depth: DepthA, env: Env, exp: SExpr, k: K[SExpr]): Res = Bounce(() => exp match {
 
-  def transformExp(depth: Int, env: Env, exp: SExpr, k: K[SExpr]): Res = exp match {
+    case atom: SExprAtomic => k (depth, relocateA(depth,env)(atom))
 
-    case x: SExprAtomic => k (env,x)
-    case x: SEVal => k(env, x)
-    case x: SEBuiltinRecursiveDefinition => k(env, x)
+    case x: SEVal => k(depth, x)
+    case x: SEBuiltinRecursiveDefinition => k(depth, x)
 
     case SEAppGeneral(func, args) =>
-      atomizeExp(depth, env, func, { case (env, func) =>
-        atomizeExps(depth, env, args.toList, { case (env, args) =>
-          val func1 = relocateAtom(depth,env)(func)
-          val args1 = args.map(relocateAtom(depth,env)).toArray
-          k(env, SEAppAtomic(func1, args1))
+      atomizeExp(depth, env, func, { case (depth, func) =>
+        atomizeExps(depth, env, args.toList, { case (depth, args) =>
+          val func1 = makeRelativeA(depth)(func)
+          val args1 = args.map(makeRelativeA(depth))
+          k(depth, SEAppAtomic(func1, args1.toArray))
         })
       })
 
@@ -152,38 +163,35 @@ object Anf {
       throw CompilationError(s"flatten: unexpected: $x")
 
     case SEMakeClo(fvs0, arity, body0) =>
-      val fvs = fvs0.map(remapLocation(depth,env))
+      val fvs = fvs0.map(relocateL(depth,env))
       val body = flattenEntry(body0)
-      k(env, SEMakeClo(fvs, arity, body))
+      k(depth, SEMakeClo(fvs, arity, body))
 
     case SECase(scrut, alts0) =>
-      atomizeExp(depth, env, scrut, { case (env, scrut) =>
-        val scrut1 = relocateAtom(depth,env)(scrut)
+      atomizeExp(depth, env, scrut, { case (depth, scrut) =>
+        val scrut1 = makeRelativeA(depth)(scrut)
         val alts = flattenAlts(depth, env, alts0)
-        k(env, SECase(scrut1, alts))
+        k(depth, SECase(scrut1, alts))
       })
 
     case SELet(rhss, body) =>
       transformLets(depth, env, rhss.toList, body, k)
 
-    case SECatch(body, handler0, fin0) => // handler & fin are always just True/False
-      atomizeExp(depth, env, body, { case (env, body) =>
-        val body1 = relocateAtom(depth,env)(body)
-        val handler = flattenExp(depth,env,handler0)
-        val fin = flattenExp(depth,env,fin0)
-        k(env, SECatch(body1, handler, fin))
-      })
+    case SECatch(body0, handler0, fin0) =>
+      val body = flattenExp(depth, env, body0)
+      val handler = flattenExp(depth,env,handler0)
+      val fin = flattenExp(depth,env,fin0)
+      k(depth, SECatch(body, handler, fin))
 
     case SELocation(loc, body) =>
-      atomizeExp(depth, env, body, { case (env, body) =>
-        val body1 = relocateAtom(depth,env)(body)
-        k(env, SELocation(loc, body1))
+      atomizeExp(depth, env, body, { case (depth, body) =>
+        val body1 = makeRelativeA(depth)(body)
+        k(depth, SELocation(loc, body1))
       })
 
     case SELabelClosure(label, exp) =>
-      atomizeExp(depth, env, exp, { case (env, exp) =>
-        val exp1 = relocateAtom(depth,env)(exp)
-        k(env, SELabelClosure(label, exp1))
+      transformExp(depth, env, exp, { case (depth, exp) =>
+        k(depth, SELabelClosure(label, exp))
       })
 
     case x: SEAbs => throw CompilationError(s"flatten: unexpected: $x")
@@ -191,29 +199,30 @@ object Anf {
     case x: SEImportValue => throw CompilationError(s"flatten: unexpected: $x")
     case x: SEVar => throw CompilationError(s"flatten: unexpected: $x")
 
-  }
+  })
 
-
-  def atomizeExps(depth: Int, env: Env, exps: List[SExpr], k: K[List[Atom]]): Res = exps match {
-    case Nil => k(env,Nil)
+  def atomizeExps(depth: DepthA, env: Env, exps: List[SExpr], k: K[List[AbsAtom]]): Res = exps match {
+    case Nil => k(depth,Nil)
     case exp::exps =>
-      atomizeExp(depth, env, exp, { case (env,atom) =>
-        atomizeExps(depth, env, exps, { case (env,atoms) =>
-          k(env, atom::atoms)
+      Bounce(() =>
+        atomizeExp(depth, env, exp, { case (depth,atom) =>
+          atomizeExps(depth, env, exps, { case (depth,atoms) =>
+            Bounce(() =>
+              k(depth, atom::atoms)
+            )
+          })
         })
-      })
+      )
   }
 
-  def atomizeExp(depth: Int, env: Env, exp: SExpr, k: K[Atom]): Res = {
-    //println(s"atomizeExp(depth=$depth,env=$env,exp=$exp")
+  def atomizeExp(depth: DepthA, env: Env, exp: SExpr, k: K[AbsAtom]): Res = {
     exp match {
-      case atom: SExprAtomic =>
-        k (env, Left(atom))
+      case ea: SExprAtomic => k (depth, makeAbsoluteA(env,ea))
       case _ =>
-        transformExp(depth, env, exp, { case (env, exp) =>
-          val (env1,newBinding) = makeNewBinding(env)
-          //println(s"atomizeExp/makeNewBinding(exp=$exp) -> env1=$env1, newbinding=$newBinding")
-          SELet(Array(exp), k(env1, Right(newBinding)))
+        transformExp(depth, env, exp, { case (depth, anf) =>
+          val atom = Right(AbsBinding(depth))
+          val body = k (DepthA(depth.n+1), atom).bounce
+          Land(SELet(Array(anf), body))
         })
     }
   }
